c375f606cfee315c9b6a96e693e95de79641d9b9
pauloviktwr <wvictor3011@gmail.com>
2024-02-07 17:09:10 -0300

    html input com metodo delete, controller DeleteMapping

--- src/main/java/com/portfolio/livros/controller/LivroController.java
+++ src/main/java/com/portfolio/livros/controller/LivroController.java
@@ -6,6 +6,7 @@ import com.portfolio.livros.model.livro.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,4 +37,14 @@ public class LivroController {
         repository.save(livro);
         return "redirect:/livros";
     }
+
+    @DeleteMapping
+    public String deleteLivro(Long id){
+
+        repository.deleteById(id);
+        System.out.println("excluido");
+
+        return "redirect:/livros";
+    }
+
 }
