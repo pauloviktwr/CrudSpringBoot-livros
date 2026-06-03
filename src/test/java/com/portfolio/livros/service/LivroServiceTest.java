4aef71a783741765eec08121d34fcd823f4ed4a0
victor r. dos santos <wvictor3011@gmail.com>
2026-06-02 23:09:20 -0300

    refactor(test): remove unused import from LivroServiceTest

--- src/test/java/com/portfolio/livros/service/LivroServiceTest.java
+++ src/test/java/com/portfolio/livros/service/LivroServiceTest.java
@@ -5,7 +5,6 @@ import com.portfolio.livros.model.dto.DadosCadastraLivro;
 import com.portfolio.livros.model.dto.DadosEditarLivro;
 import com.portfolio.livros.repository.LivroRepository;
 
-import jakarta.persistence.EntityNotFoundException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -26,7 +25,6 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 
